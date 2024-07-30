4f831dca9e34185bf694f2413fbcc5bea2a0c1b9
shaquille0520 <31034222+shaquille0520@users.noreply.github.com>
2024-07-31 00:57:46 +0100

    Add files via upload

---
+++ Robosapien-master/test.py
@@ -0,0 +1,36 @@
+# by Carl Monk (@ForToffee)
+# github.com/ForToffee/Robosapien
+
+# command codes from http://www.aibohack.com/robosap/ir_codes.htm
+import robo
+
+rs = robo.Robo(21)  # create Robo object for GPIO 21
+rs.send_code(0xB1)  # Issue reset command
+input('Enter')
+rs.send_code(0x81)  # Right arm up
+rs.send_code(0x81)
+rs.send_code(0x82)  # Right wrist out
+rs.send_code(0x85)  # Right wrist in
+rs.send_code(0x82)
+rs.send_code(0x85)
+
+input('Enter')
+rs.send_code(0x89)  # Left arm up
+rs.send_code(0x89)
+for i in range(0, 2):
+    rs.send_code(0x8B)  # Tilt left
+    rs.send_code(0x83)  # Tilt right
+    rs.send_code(0x83)
+    rs.send_code(0x8B)
+rs.send_code(0x8C)  # Left arm down
+rs.send_code(0x84)  # Right arm down
+rs.send_code(0x8C)
+rs.send_code(0x84)
+
+input('Enter')
+rs.send_code(0xC4)  # Hi 5
+
+input('Enter')
+rs.send_code(0xCE)  # Roar
+
+print("fin")
